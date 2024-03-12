e77d2f273a6b8cc1d647b7acf30818eb55b0f72f
Martin Santamaria <msantamaria@mvdcreativestudio.com>
2024-03-12 12:04:01 -0300

    QA V1.0

--- resources/views/content/accounting/entries.blade.php
+++ resources/views/content/accounting/entries.blade.php
@@ -73,7 +73,7 @@
           <th>Diario Principal</th>
           <th>Cierre Febrero</th>
           <th>USD</th>
-          <th>1849</th>
+          <th>18490</th>
           <th><a href="entrie"><i class="bx bx-right-arrow-alt"></i></a></th>
         </tr>
       </tbody>
