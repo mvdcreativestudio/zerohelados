b0669f373ec0d2742fe4a25f7c686bae2d58267a
Martin Santamaria <msantamaria@mvdcreativestudio.com>
2024-04-18 12:25:45 -0300

    modificacion rutas

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
+use App\Http\Controllers\language\LanguageController;
 use App\Http\Controllers\{
     RawMaterialController, EcommerceController, OmnichannelController, CrmController, InvoiceController,
     ClientController, AccountingController, StoreController, RoleController, SupplierController, SupplierOrderController,
-    ProductController, ProductCategoryController, OrderController, CartController, CheckoutController, MercadoPagoController, CouponController};
-
+    ProductController, ProductCategoryController, OrderController, CartController, CheckoutController, MercadoPagoController, CouponController
+};
 
 // Autenticación y Verificación de Email
 Route::middleware([
@@ -19,7 +18,7 @@ Route::middleware([
         return view('content.dashboard.dashboard-mvd');
     })->name('dashboard');
 
-    // Dashboard Data Tables
+    // Data Tables
     Route::get('/clients/datatable', [ClientController::class, 'datatable'])->name('clients.datatable');
     Route::get('/products/datatable', [ProductController::class, 'datatable'])->name('products.datatable');
     Route::get('/product-categories/datatable', [ProductCategoryController::class, 'datatable'])->name('product-categories.datatable');
@@ -27,7 +26,6 @@ Route::middleware([
     Route::get('/orders/{order}/datatable', [OrderController::class, 'orderProductsDatatable'])->name('order-products.datatable');
     Route::get('/marketing/coupons/datatable', [CouponController::class, 'datatable'])->name('coupons.datatable');
     Route::get('/products/flavors/datatable', [ProductController::class, 'flavorsDatatable'])->name('products.flavors.datatable');
-
 
     // Recursos con acceso autenticado
     Route::resources([
@@ -42,21 +40,21 @@ Route::middleware([
         'orders' => OrderController::class,
         'invoices' => InvoiceController::class,
         '/marketing/coupons' => CouponController::class,
+        'checkout' => CheckoutController::class
     ]);
 
-
-    // Products
+    // Product management
     Route::get('products/{id}/duplicate', [ProductController::class, 'duplicate'])->name('products.duplicate');
     Route::post('products/{id}/switchStatus', [ProductController::class, 'switchStatus'])->name('products.switchStatus');
 
-    // Stores
+    // Store management
     Route::prefix('stores/{store}')->name('stores.')->group(function () {
         Route::get('manage-users', [StoreController::class, 'manageUsers'])->name('manageUsers');
         Route::post('associate-user', [StoreController::class, 'associateUser'])->name('associateUser');
         Route::post('disassociate-user', [StoreController::class, 'disassociateUser'])->name('disassociateUser');
     });
 
-    // Roles
+    // Role management
     Route::prefix('roles/{role}')->name('roles.')->group(function () {
         Route::get('manage-users', [RoleController::class, 'manageUsers'])->name('manageUsers');
         Route::post('associate-user', [RoleController::class, 'associateUser'])->name('associateUser');
@@ -65,55 +63,52 @@ Route::middleware([
         Route::post('assign-permissions', [RoleController::class, 'assignPermissions'])->name('assignPermissions');
     });
 
-    // Variaciones
+    // Flavor management
     Route::get('product-flavors', [ProductController::class, 'flavors'])->name('product-flavors');
     Route::post('product-flavors', [ProductController::class, 'storeFlavors'])->name('product-flavors.store');
     Route::post('/product-flavors/multiple', [ProductController::class, 'storeMultipleFlavors'])->name('product-flavors.store-multiple');
     Route::delete('product-flavors/{id}/delete', [ProductController::class, 'destroyFlavor'])->name('product-flavors.destroy');
     Route::put('flavors/{id}/switch-status', [ProductController::class, 'switchFlavorStatus'])->name('flavors.switch-status');
 
-    // CRM, Contabilidad y Otros
+    // CRM, Accounting
     Route::get('crm', [CrmController::class, 'index'])->name('crm');
     Route::get('receipts', [AccountingController::class, 'receipts'])->name('receipts');
     Route::get('entries', [AccountingController::class, 'entries'])->name('entries');
     Route::get('entrie', [AccountingController::class, 'entrie'])->name('entrie');
 
-    // E-Commerce Backoffice
+    // E-Commerce Settings
     Route::get('/ecommerce/marketing', [EcommerceController::class, 'marketing'])->name('marketing');
     Route::get('/ecommerce/settings', [EcommerceController::class, 'settings'])->name('settings');
-    // Orders
+
+    // Order details
     Route::get('/orders/{order}/show', [OrderController::class, 'show'])->name('orders.show');
-    // Cupones
+
+    // Coupon management
     Route::post('marketing/coupons/delete-selected', [CouponController::class, 'deleteSelected'])->name('coupons.deleteSelected');
     Route::get('coupons/{id}', [CouponController::class, 'show'])->name('coupons.show');
-    // Sabores
+
+    // Flavor editing
     Route::get('/flavors/{id}', [ProductController::class, 'editFlavor'])->name('flavors.edit');
     Route::put('/flavors/{id}', [ProductController::class, 'updateFlavor'])->name('flavors.update');
-
 });
 
-
-// Rutas de E-Commerce (Públicas)
+// Public E-Commerce Routes
 Route::get('shop', [EcommerceController::class, 'index'])->name('shop');
 Route::get('store/{storeId}', [EcommerceController::class, 'store'])->name('store');
 Route::post('/cart/select-store', [CartController::class, 'selectStore'])->name('cart.selectStore');
 Route::post('/cart/add/{productId}', [CartController::class, 'addToCart'])->name('cart.add');
 Route::get('/session/clear', [CartController::class, 'clearSession'])->name('session.clear');
-Route::resource('checkout', CheckoutController::class);
 Route::get('/checkout/{orderId}/payment', [CheckoutController::class, 'payment'])->name('checkout.payment');
 Route::get('/checkout/success/{order}', [CheckoutController::class, 'success'])->name('checkout.success');
-
-// Cupones de descuento
-Route::post('/apply-coupon', [CheckoutController::class, 'applyCoupon'])->name('apply.coupon');
-
-
 Route::get('/success/{orderId}', [CheckoutController::class, 'success'])->name('checkout.success');
 Route::get('/pending', [CheckoutController::class, 'pending'])->name('checkout.pending');
 Route::get('/failure', [CheckoutController::class, 'failure'])->name('checkout.failure');
+Route::post('/apply-coupon', [CheckoutController::class, 'applyCoupon'])->name('apply.coupon');
 
-
-// Omnicanalidad (Público)
+// Omnichannel public access
 Route::get('omnichannel', [OmnichannelController::class, 'index'])->name('omnichannel');
 
 // MercadoPago WebHooks
 Route::post('/mpagohook', [MercadoPagoController::class, 'webhooks'])->name('mpagohook');
+
+Route::get('lang/{locale}', [LanguageController::class, 'swap']);
