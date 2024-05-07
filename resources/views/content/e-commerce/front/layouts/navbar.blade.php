6ff0cfa424888270ebe83118631367475d3f924e
Martin Santamaria <msantamaria@mvdcreativestudio.com>
2024-05-07 00:59:10 -0300

    cambio de tienda fixeado

--- resources/views/content/e-commerce/front/layouts/navbar.blade.php
+++ resources/views/content/e-commerce/front/layouts/navbar.blade.php
@@ -1,3 +1,7 @@
+@php
+  $stores = App\Models\Store::all();
+@endphp
+
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
     <img src="{{ asset('assets/img/branding/chelato-white.png') }}" alt="" class="navbar-logo">
@@ -8,7 +12,9 @@
       <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
         @if(session('store'))
         <li class="nav-item">
-          <a class="nav-link nav-link-menu" aria-current="page" href="{{ route('shop') }}"><i class="fa-solid fa-location-dot"></i>     {{ session('store')['name'] }}</a>
+          <a class="nav-link nav-link-menu" href="#" data-bs-toggle="modal" data-bs-target="#selectStoreModal">
+            <i class="fa-solid fa-location-dot"></i> {{ session('store')['name'] }}
+          </a>
         </li>
         @endif
       </ul>
@@ -20,7 +26,7 @@
           <a href="" class="nav-link nav-link-menu"><i class="fa-regular fa-user"></i></a>
         </li>
         <li>
-          <a href="" class="nav-link nav-link-menu" data-bs-toggle="offcanvas" data-bs-target="#offcanvasEnd"><i class="fa-solid fa-cart-shopping"></i></a>
+          <a href="#" class="nav-link nav-link-menu" onclick="confirmChangeStore()"><i class="fa-solid fa-cart-shopping"></i> Cambiar de tienda</a>
         </li>
         <li>
           <a href="" class="nav-link nav-link-menu"><i class="fa-solid fa-bars"></i></a>
@@ -29,3 +35,44 @@
     </div>
   </div>
 </nav>
+
+<!-- Modal -->
+<div class="modal fade" id="selectStoreModal" tabindex="-1" aria-labelledby="selectStoreModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="selectStoreModalLabel">Cambiar de tienda</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body pt-2">
+        <div class="alert alert-danger" role="alert">
+          Perderás los productos que hayas agregado al carrito.
+        </div>
+        <form id="changeStoreForm" action="{{ route('cart.selectStore') }}" method="POST">
+          @csrf
+          <div class="mb-3">
+            <label for="storeId" class="form-label">Seleccionar tienda:</label>
+            <select class="form-select" name="storeId" id="storeId">
+              @foreach($stores as $store)
+              <option value="{{ $store->id }}">{{ $store->name }}</option>
+              @endforeach
+            </select>
+          </div>
+          <button type="submit" class="btn btn-primary">Cambiar tienda</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<script>
+  function confirmChangeStore() {
+    $('#selectStoreModal').modal('show');
+  }
+
+  // Reinicializar el formulario de cambio de tienda cada vez que se muestre el modal
+  $('#selectStoreModal').on('show.bs.modal', function () {
+    $('#changeStoreForm')[0].reset(); // Reiniciar el formulario
+  });
+</script>
