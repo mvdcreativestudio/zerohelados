b5d0a8d67cc77f4775ace34b514aae6b7b3298da
sparadeloweb <santiagoparadelo123@gmail.com>
2024-09-05 01:05:02 -0300

    Rate USD

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command('stores:check-hours')->everyMinute();
+        $schedule->command('fetch:currencies-rates')->daily();
     }
 
     /**
