0dbdc381099194861f5b64142241818826257196
Martin Santamaria <msantamaria@mvdstudio.com.uy>
2024-09-17 00:48:26 -0300

    Avanzando con MP

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -64,6 +64,9 @@ class CheckoutController extends Controller
         // Obtener el ID de la tienda desde la sesión
         $storeId = session('store.id');
 
+        // Agregar Log para verificar el store_id
+        Log::info('Valor de store_id en la sesión al llegar al checkout:', ['store_id' => $storeId]);
+
         // Verificar si el storeId es numérico
         if (!is_numeric($storeId)) {
             Log::error('El store_id en la sesión no es numérico: ' . $storeId);
@@ -71,14 +74,12 @@ class CheckoutController extends Controller
             abort(404, 'Store ID inválido en la sesión');
         }
 
-        Log::info('ID de la tienda almacenado en la sesión: ' . session('store.id'));
-
-
         // Añadir el ID de la tienda a los datos que se pasan a la vista
         $checkout['store_id'] = $storeId;
 
         return view('content.e-commerce.front.checkout', $checkout);
     }
+
 
 
     /**
