7b6cc57ff93b012dd1bb41ac52f717b34e2b100a
Marcos Franco <mfranco@mvdstudio.com.uy>
2024-09-20 14:02:13 -0300

    Cambios de mergeo

--- config/variables.php
+++ config/variables.php
@@ -3,7 +3,7 @@
   return [
     "creatorName" => "MDV Studio",
     "creatorUrl" => "#",
-    "templateName" => "Craft",
+    "templateName" => "Base",
     "templateSuffix" => "",
     "templateVersion" => "2.2.0",
     "templateFree" => false,
